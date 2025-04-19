385aacca629d5a1f1b2af632ac46228540996307
GasGoust <134607493+GasGoust@users.noreply.github.com>
2025-04-19 05:30:52 +0300

    Lecture 17 (JS - 2)
    Lecture 17 (JS - 2)

---
+++ Lecture 17 (JS - 2)/JS.js
@@ -0,0 +1,23 @@
+// to String
+String(value)
+let srt = String(null) = "null"
+let strNum = String(8) = "8"
+
+// Kontatenation
+let str = 5 + "" // = 5
+let strBool = false + "" // false
+
+// to Number
+Number(value) // Какое то количество чего либо
+let num = Number("8") // = 8
+
+// Unitar +
+let num = +"8" // string 8
+let numNull = +null // ->0
+
+parseInt(value) \ parseFloat(value)
+let num = parseInt("15px") // -> 15
+let num = parseFloat("1.5 землекопа") // -> 1.5 Землекопа
+
+
+// Время 15.30
